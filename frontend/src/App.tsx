2bbd8feccc36259a69747653e97c484efd4aa4ee
eman112357 <65303937+eman112357@users.noreply.github.com>
2025-09-13 06:45:32 +0300

    Create src/App.tsx with initial App component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Agri-Marketplace Frontend!</h1>
+      <p>Your marketplace will be here soon.</p>
+    </div>
+  );
+}
+
+export default App;
